c75d03a5632f4bf32f14e49d426a0cae6aba37b8
Kenyo-cell <rekurt3000@gmail.com>
2022-05-31 15:17:12 +0300

    feat: extended repos with new methods

--- src/main/java/ru/blogback/repository/TagRepository.java
+++ src/main/java/ru/blogback/repository/TagRepository.java
@@ -3,5 +3,10 @@ package ru.blogback.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.blogback.entity.Tag;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, String> {
+    boolean existsByLabel(String label);
+    void deleteByLabel(String label);
+    Optional<Tag> findByLabel(String label);
 }
--- src/main/java/ru/blogback/repository/UserRepository.java
+++ src/main/java/ru/blogback/repository/UserRepository.java
@@ -3,5 +3,10 @@ package ru.blogback.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.blogback.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUsername(String username);
+    void deleteByUsername(String username);
+    Optional<User> findByUsername(String name);
 }
