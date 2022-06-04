7e8b36e191b66e41b119fbdd1204ff30716de6fc
Kenyo-cell <rekurt3000@gmail.com>
2022-06-04 21:32:13 +0300

    feat: extended configs with new authority add trigger, rsa keys and oauth server

--- src/main/resources/db/changelog/changes/authority-add.sql
+++ src/main/resources/db/changelog/changes/authority-add.sql
@@ -1,25 +1,12 @@
-CREATE OR REPLACE FUNCTION users_clear_likes_and_posts()
+CREATE OR REPLACE FUNCTION add_default_authority()
     RETURNS trigger AS '
     BEGIN
-        DELETE FROM likes where username == old.username;
-        RETURN OLD;
+        INSERT INTO authorities (username, role) VALUES (NEW.username, ''ROLE_USER'');
+        RETURN NEW;
     END;
 '
     LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION clear_posts_func()
-    RETURNS trigger AS '
-    BEGIN
-        DELETE FROM likes WHERE post_id == old.id;
-        RETURN OLD;
-    END;
-'
-    LANGUAGE plpgsql;
-
-CREATE TRIGGER users_clear_trigger
-    AFTER DELETE ON users
-    FOR EACH ROW EXECUTE FUNCTION users_clear_likes_and_posts();
-
-CREATE TRIGGER posts_clear_trigger
-    AFTER DELETE ON posts
-    FOR EACH ROW EXECUTE FUNCTION clear_posts_func();
+CREATE TRIGGER add_default_authority_trigger
+    AFTER INSERT ON users
+    FOR EACH ROW EXECUTE FUNCTION add_default_authority();
